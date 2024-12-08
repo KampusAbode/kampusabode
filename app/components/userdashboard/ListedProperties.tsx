13bffa9eed71176d694fd610df4de9b06622749f
ajalatolulope <84034486+AJ-Tolulope@users.noreply.github.com>
2024-12-08 20:48:34 +0100

    changes

--- app/components/userdashboard/ListedProperties.tsx
+++ app/components/userdashboard/ListedProperties.tsx
@@ -1,21 +1,40 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import { getProperties } from "../../utils/api";
-import { AgentType, PropertyType } from '../../fetch/types';
+import { AgentType, PropertyType } from "../../fetch/types";
 import Link from "next/link";
 
-
-const ListedProperties = async ({ user }: {user: AgentType}) => {
-  const { propertiesListed } = user.userInfo; 
-
-  // Filter properties based on IDs in propertiesListed
-  const fetchedProperties: PropertyType[] = await getProperties();
-  const filteredProperties = fetchedProperties.filter((property) =>
-    propertiesListed.some((listedProperty) => listedProperty.id === property.id.toString())
+const ListedProperties = ({ user }: { user: AgentType }) => {
+  const [filteredProperties, setFilteredProperties] = useState<PropertyType[]>(
+    []
   );
 
+  useEffect(() => {
+    const fetchProperties = async () => {
+      const { propertiesListed } = user.userInfo;
+
+      const fetchedProperties: PropertyType[] = await getProperties();
+      const filtered = fetchedProperties.filter((property) =>
+        propertiesListed.some(
+          (listedProperty) => listedProperty.id === property.id.toString()
+        )
+      );
+
+      setFilteredProperties(filtered);
+    };
+
+    fetchProperties();
+  }, [user]);
 
   return (
     <div className="listed-properties">
-      <h4>Your Listed Properties <Link href={"/upload"} className="btn">Upload apartment</Link></h4>
+      <h4>
+        Your Listed Properties{" "}
+        <Link href={"/upload"} className="btn">
+          Upload apartment
+        </Link>
+      </h4>
       <div className="property-list">
         <ul>
           {filteredProperties && filteredProperties.length > 0 ? (
