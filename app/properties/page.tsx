bce3f8514d836fa2658f0533e088072c0a4e1024
ajalatolulope <84034486+AJ-Tolulope@users.noreply.github.com>
2024-12-22 09:12:20 +0100

    changes

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -80,59 +80,58 @@ const PropertiesPage: React.FC = () => {
     <section className="listings-page">
       <div className="banner">
         <div className="container">
-        <span>your comfort</span>
-        <h2>
-          our prority
-          <svg
-            width="50"
-            height="49"
-            viewBox="0 0 50 49"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M24.6571 9.0258C18.75 12.9418 13.3443 17.4625 7.63462 21.628"
-              stroke="#CCD7FF"
-              strokeWidth="2.65"
-              strokeMiterlimit="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M43.1283 16.1354C31.9263 19.0499 20.869 23.231 10.2516 27.7933"
-              stroke="#CCD7FF"
-              strokeWidth="2.65"
-              strokeMiterlimit="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M38.189 35.6927C30.4675 33.8314 18.8695 34.5847 10.6475 34.622"
-              stroke="#CCD7FF"
-              strokeWidth="2.65"
-              strokeMiterlimit="1.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-        </h2>
+          <span>your comfort</span>
+          <h2>
+            our prority
+            <svg
+              width="50"
+              height="49"
+              viewBox="0 0 50 49"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M24.6571 9.0258C18.75 12.9418 13.3443 17.4625 7.63462 21.628"
+                stroke="#CCD7FF"
+                strokeWidth="2.65"
+                strokeMiterlimit="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M43.1283 16.1354C31.9263 19.0499 20.869 23.231 10.2516 27.7933"
+                stroke="#CCD7FF"
+                strokeWidth="2.65"
+                strokeMiterlimit="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M38.189 35.6927C30.4675 33.8314 18.8695 34.5847 10.6475 34.622"
+                stroke="#CCD7FF"
+                strokeWidth="2.65"
+                strokeMiterlimit="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </h2>
         </div>
 
-        <div className="dots">
-          <Image
-            width={109}
-            height={251.8}
-            src="/assets/Dots.png"
-            alt="dots"
-          />
-        </div>
-        <div className="building">
-          <Image
-            width={387}
-            height={253}
-            src="/assets/Building.svg"
-            alt="buildings"
-          />
-        </div>
+        <Image
+          className="dots"
+          width={109}
+          height={251.8}
+          src="/assets/Dots.png"
+          alt="dots"
+        />
+
+        <Image
+          className="building"
+          width={387}
+          height={253}
+          src="/assets/Building.svg"
+          alt="buildings"
+        />
       </div>
 
       <div className="user-search">
