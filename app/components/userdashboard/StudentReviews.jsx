25f8d339f00335a14d125c248eaf42301aa2cef1
ajalatolulope <84034486+AJ-Tolulope@users.noreply.github.com>
2025-01-07 10:00:34 +0100

    Refactor Nav and StudentReviews components: simplify SVG stroke handling and improve review rendering logic

--- app/components/userdashboard/StudentReviews.jsx
+++ app/components/userdashboard/StudentReviews.jsx
@@ -4,25 +4,29 @@ import toast from "react-hot-toast";
 const StudentReviews = ({ user }) => {
   // Filter reviews by userId
   let setPropReviews;
-     (() => {
-      const fetchReviews = async () => {
-        try {
-          const fetchedReviews = await fetchReviewsByAgentId(user?.id);
-          
-          setPropReviews = fetchedReviews;
-        } catch {
-          toast.error("Failed to fetch reviews.");
-        }
-      };
-      fetchReviews();
-    });
+  () => {
+    const fetchReviews = async () => {
+      try {
+        const fetchedReviews = await fetchReviewsByAgentId(user?.id);
+
+        setPropReviews = fetchedReviews;
+      } catch {
+        toast.error("Failed to fetch reviews.");
+      }
+    };
+    fetchReviews();
+  };
 
   return (
     <div className="student-reviews">
       <h3>Student Reviews</h3>
       <ul>
         {setPropReviews.length > 0 ? (
-          setPropReviews.map((review) => <li key={review.id}>{review.content} - {review.author}</li>)
+          setPropReviews.map((review) => (
+            <li key={review.id}>
+              {review.content} - {review.author}
+            </li>
+          ))
         ) : (
           <li>No reviews found.</li>
         )}
