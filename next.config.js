333b7c633d68d8dd78d47b6f2d4a76f2537faa0f
ajalatolulope <84034486+AJ-Tolulope@users.noreply.github.com>
2025-01-30 03:47:05 +0100

    Update Unsplash hostname in next.config.js for image loading

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "www.unsplash.com",
+        hostname: "images.unsplash.com",
         pathname: "/**",
       },
       {
