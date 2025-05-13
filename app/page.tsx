00821f93a97d5647c54e489d38282ae47c62b08c
aj-tolulope <tolulopetimilehin124@gmail.com>
2025-05-13 21:33:56 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { TrendType } from "./fetch/types";
 import { allTrends, useProperties } from "./utils";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { RiVerifiedBadgeLine } from "react-icons/ri";
 import { useState, useEffect } from "react";
 // Import the Zustand store for properties
 import { useUserStore } from "./store/userStore";
@@ -166,7 +167,7 @@ export default function App() {
             {testimonials.map((testi) => (
               <div key={testi.author} className="testimonial-card">
                 <div className="details">
-                  <div>
+                  <div className="testi-profile">
                     <Image
                       priority
                       src={testi.image}
@@ -180,23 +181,9 @@ export default function App() {
                     </div>
                   </div>
 
-                  {testi.rating && (
-                    <div className="rating">
-                      {Array.from({ length: testi.rating }, (_, i) => (
-                        <span key={i}>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            id="Outline"
-                            viewBox="0 0 24 24"
-                            width="24"
-                            height="24"
-                            fill="#f09036">
-                            <path d="M23.836,8.794a3.179,3.179,0,0,0-3.067-2.226H16.4L15.073,2.432a3.227,3.227,0,0,0-6.146,0L7.6,6.568H3.231a3.227,3.227,0,0,0-1.9,5.832L4.887,15,3.535,19.187A3.178,3.178,0,0,0,4.719,22.8a3.177,3.177,0,0,0,3.8-.019L12,20.219l3.482,2.559a3.227,3.227,0,0,0,4.983-3.591L19.113,15l3.56-2.6A3.177,3.177,0,0,0,23.836,8.794Zm-2.343,1.991-4.144,3.029a1,1,0,0,0-.362,1.116L18.562,19.8a1.227,1.227,0,0,1-1.895,1.365l-4.075-3a1,1,0,0,0-1.184,0l-4.075,3a1.227,1.227,0,0,1-1.9-1.365L7.013,14.93a1,1,0,0,0-.362-1.116L2.507,10.785a1.227,1.227,0,0,1,.724-2.217h5.1a1,1,0,0,0,.952-.694l1.55-4.831a1,1,0,0,1,2.336,0l1.55,4.831a1,1,0,0,0,.952.694h5.1a1.227,1.227,0,0,1,.724,2.217Z" />
-                          </svg>
-                        </span>
-                      ))}
-                    </div>
-                  )}
+                  <div className="rating">
+                    <RiVerifiedBadgeLine />
+                  </div>
                 </div>
                 <div className="testi-content">
                   <span className="testi">{testi.testimonial}</span>
