d349f1d1106e7bc9454eb5985af61aa414c1ac64
Tolulope Ajala <84034486+AJ-Tolulope@users.noreply.github.com>
2025-06-24 12:34:41 +0100

    Update QuickService.tsx

--- app/components/quickservice/QuickService.tsx
+++ app/components/quickservice/QuickService.tsx
@@ -34,6 +34,11 @@ const QuickService = () => {
       icon: BsChatRightText,
       link: `/chat/${user.id}/${user.name}`,
     };
+  } else if  (pathname === "/trends" && checkIsAdmin(user.id) {
+    config = {
+      icon: FaPlus,
+      link: "/trends/upload",
+    }
   }
 
   if (!config) return null;
