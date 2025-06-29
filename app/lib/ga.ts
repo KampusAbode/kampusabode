020f177fd8938ff0875ff7cd2fec64b9152125ac
Tolulope Ajala <84034486+AJ-Tolulope@users.noreply.github.com>
2025-06-29 05:41:19 +0100

    Update ga.ts

--- app/lib/ga.ts
+++ app/lib/ga.ts
@@ -1,1 +1,10 @@
 
+// lib/ga.ts
+export const GA_MEASUREMENT_ID = 'G-LTGR69WRJB'; // Replace with yours
+
+// Optional: track pageviews manually
+export const pageview = (url: string) => {
+  window.gtag('config', GA_MEASUREMENT_ID, {
+    page_path: url,
+  });
+};
