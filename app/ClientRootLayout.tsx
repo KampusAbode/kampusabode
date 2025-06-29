21484da03a4bff86627dab66c35af817a33f57b2
Tolulope Ajala <84034486+AJ-Tolulope@users.noreply.github.com>
2025-06-29 04:54:06 +0100

    Update ClientRootLayout.tsx

--- app/ClientRootLayout.tsx
+++ app/ClientRootLayout.tsx
@@ -5,6 +5,9 @@ import Navigation from "./components/navigation/Navigation";
 import Nav from "./components/navMenu/nav";
 import QuickService from "./components/quickservice/QuickService";
 import { useUserStore } from "./store/userStore";
+import { useEffect } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import { saveScroll, getScroll } from './scrollManager';
 import Loader from "./components/loader/Loader";
 import { useAuthListener } from "./hooks/useAuthListener";
 
@@ -28,6 +31,41 @@ function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
   if (initializing) {
     return <Loader />;
   }
+
+
+  const pathname = usePathname();
+  const router = useRouter();
+
+  useEffect(() => {
+    let shouldRestore = false;
+
+    const handleBeforeUnload = () => {
+      saveScroll(pathname);
+    };
+
+    const handlePopState = () => {
+      shouldRestore = true;
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    window.addEventListener('popstate', handlePopState);
+
+    return () => {
+      saveScroll(pathname);
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+      window.removeEventListener('popstate', handlePopState);
+    };
+  }, [pathname]);
+
+  useEffect(() => {
+    // Defer scroll restoration
+    requestAnimationFrame(() => {
+      const savedScroll = getScroll(pathname);
+      if (savedScroll !== undefined) {
+        window.scrollTo(0, savedScroll);
+      }
+    });
+  }, [pathname]);
 
   return (
     <div className={`wrapper `}>
