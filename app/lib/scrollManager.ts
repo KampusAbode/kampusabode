0f207f55bc5d8b903921ed9666c6c2f7aa27946d
Tolulope Ajala <84034486+AJ-Tolulope@users.noreply.github.com>
2025-06-29 04:53:04 +0100

    Update scrollManager.ts

--- app/lib/scrollManager.ts
+++ app/lib/scrollManager.ts
@@ -1,1 +1,11 @@
 
+// lib/scrollManager.ts
+const scrollPositions = new Map<string, number>();
+
+export function saveScroll(key: string) {
+  scrollPositions.set(key, window.scrollY);
+}
+
+export function getScroll(key: string): number | undefined {
+  return scrollPositions.get(key);
+}
