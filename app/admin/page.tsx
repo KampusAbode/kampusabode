b3e323b49688ad30d302dcb7d3a3a452fdd2d743
Tolulope Ajala <84034486+AJ-Tolulope@users.noreply.github.com>
2025-10-04 06:07:07 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 export default function AdminPage() {
   const router = useRouter();
 
+  
   useEffect(() => {
     // Redirect to users page by default
     router.push("/admin/users");
