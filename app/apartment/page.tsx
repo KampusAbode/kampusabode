e2e256464b1ef094c9fea8a887b26c1289f98f37
Tolulope Ajala <84034486+AJ-Tolulope@users.noreply.github.com>
2025-10-05 10:49:55 +0100

    Update page.tsx

--- app/apartment/page.tsx
+++ app/apartment/page.tsx
@@ -8,7 +8,7 @@ import Loader from "../components/loader/Loader";
 import "./apartment.css";
 import { FaSearch } from "react-icons/fa";
 import { useSearchParams, useRouter } from "next/navigation";
-import { useScrollRestoration } from "@/hooks/useScrollRestoration";
+import { useScrollRestoration } from "../hooks/useScrollRestoration";
 import { usePropertiesStore } from "../store/propertiesStore";
 import data from "../fetch/contents";
 
