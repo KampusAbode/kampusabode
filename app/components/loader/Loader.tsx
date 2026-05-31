b6700ef8f85b1944e8b6d949c2c5b978775a0c68
aj-tolulope <tolulopetimilehin124@gmail.com>
2026-05-31 20:25:14 +0100

    style: format Loader component and CSS for consistency

--- app/components/loader/Loader.tsx
+++ app/components/loader/Loader.tsx
@@ -3,11 +3,11 @@ import "./Loader.css";
 
 const Loader: FC = () => {
   return (
-<div className="spinner">
-<svg viewBox="25 25 50 50">
-  <circle r="20" cy="50" cx="50"></circle>
-</svg>
-</div>
+    <div className="spinner">
+      <svg viewBox="25 25 50 50">
+        <circle r="20" cy="50" cx="50"></circle>
+      </svg>
+    </div>
   );
 };
 
